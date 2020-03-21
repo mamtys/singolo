3919e6f01f13d7b0f284754504ef2af125b8cf72
mamtys <Mamtysman@gmail.com>
2020-03-21 23:33:19 +0300

    feat: add slider events
    add slider carousel event
    add phone toggle screen on click
    add portfolio shufle on tag click

--- script.js
+++ script.js
@@ -13,6 +13,14 @@
         event.target.classList.add('nav-active');
 
         active = event.target;
+        
+
+        classes = shuffle(PORTFOLIO_IMG_ClASSES);
+        [...portfolioImages.children].forEach((element, ind) => {
+            element
+                .querySelector('.portfolio-example-item')
+                .className = `portfolio-example-item ${classes[ind]}`;
+        });
     })
 })();
 
@@ -83,6 +91,76 @@
     })
 })();
 
+
+(function () {
+    let slider = document.getElementById('slider');
+    let sliders = slider.getElementsByClassName('slider-item');
+    let counter = sliders.length - 1;
+    let transitionEnded = true;
+    let prev;
+    let direction;
+
+    slider.addEventListener('click', (event) => {
+        toggleChildClass('.phone', '.phone-screen', 'phone-screen-active', event);
+
+        if (!transitionEnded) {
+            return;
+        }
+
+        if (event.target.matches('.slider-arrow')) {
+            direction = Number(event.target.dataset['direction']);
+            prev = sliders[counter];
+
+            prev.style.transitionDuration = '1.5s';
+            prev.style.transform += `translateX(${direction*100}%)`;
+            prev.style.zIndex = 5;
+
+            counter = direction + counter;
+            if (counter < 0) {
+                counter = sliders.length - 1;
+            }
+            if (counter > sliders.length - 1) {
+                counter = 0;
+            }
+
+            sliders[counter].style.left = `${-direction*100}%`;
+            sliders[counter].style.transitionDuration = '1.5s';
+            sliders[counter].style.transform += `translateX(${direction*100}%)`;
+            sliders[counter].style.zIndex = 5;
+
+            transitionEnded = false;
+        }
+    })
+
+    slider.addEventListener('transitionend', (event) => {
+        transitionEnded = true;
+        prev.style = {};
+        prev.style.zIndex = 0;
+    })
+
+})();
+
+function toggleClass(targetSelector, toggleClass, event) {
+
+    if (!event.target.matches(targetSelector)) {
+        return false;
+    }
+    event.target.classList.toggle(toggleClass);
+    return true;
+}
+
+function toggleChildClass(parentSelector, targetSelector, toggleClass, event) {
+    if (!event.target.closest(parentSelector)) {
+        return;
+    }
+
+    if (event.target.matches(targetSelector)) {
+        event.target.classList.toggle(toggleClass);
+    } else {
+        event.target.querySelector(targetSelector).classList.toggle(toggleClass);
+    }
+}
+
 function createPopUp(subject = 'No Subject', description = 'No Description', name, cb) {
     const popup = document.createElement('template');
     const html = `
@@ -113,3 +191,11 @@ function createPopUp(subject = 'No Subject', description = 'No Description', nam
 
     return popup.content.firstElementChild;
 }
+
+
+function shuffle(array) {
+    return [...array].sort(() => (Math.random() - 0.5));
+}
+
+
+const PORTFOLIO_IMG_ClASSES = [...Array(12)].map((el, ind) => `portfolio-img${ind + 1}`);
